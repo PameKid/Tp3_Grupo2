2b294a85843e3190257b4dbcc1ebbedae57ca1c4
Kimblag <kimberly.blandong@gmail.com>
2026-04-25 14:18:30 -0300

    fix(dao): Se corrige método obtenerCategorias y se cierran: resulset y statement para evitar dejar conexiones abiertas en memoria.

--- src/dao/DaoCategoria.java
+++ src/dao/DaoCategoria.java
@@ -12,34 +12,27 @@ import entidad.Categoria;
 public class DaoCategoria {
 
 	public int agregarCategoria(Categoria c) {
-		
-	    int filas = 0;
-	    String query = "INSERT INTO categorias (Nombre) VALUES (?)";
-	    PreparedStatement pst = null;
-	    
-	    try
-	    {
-	    	pst = Conexion.getInstancia().getConnection().prepareStatement(query);
-	        pst.setString(1, c.getNombre());
-	        filas = pst.executeUpdate();
-	    } 
-	    catch (SQLException ex) 
-	    {
-	        ex.printStackTrace();
-	    }
-	    finally 
-	    {
-			try 
-			{
-				if(pst!=null) pst.close();
-			} 
-			catch (SQLException ex) 
-			{
+
+		int filas = 0;
+		String query = "INSERT INTO categorias (Nombre) VALUES (?)";
+		PreparedStatement pst = null;
+
+		try {
+			pst = Conexion.getInstancia().getConnection().prepareStatement(query);
+			pst.setString(1, c.getNombre());
+			filas = pst.executeUpdate();
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		} finally {
+			try {
+				if (pst != null)
+					pst.close();
+			} catch (SQLException ex) {
 				ex.printStackTrace();
 			}
 		}
-	    
-	    return filas;
+
+		return filas;
 	}
 
 	public int eliminarCategoria(int idCategoria) {
@@ -68,6 +61,7 @@ public class DaoCategoria {
 
 		return filas;
 	}
+
 	
 	// Sebastian Flores
 	
@@ -105,36 +99,46 @@ public class DaoCategoria {
 	
 	
 	
-	
-	
 	//Pamela Vizcarra 
-	
-	public ArrayList<Categoria> obtenerCategorias(){
-		
+
+	public ArrayList<Categoria> obtenerCategorias() {
+
 		ArrayList<Categoria> lCategoria = new ArrayList<Categoria>();
 		Connection cn = null;
+		Statement st = null;
+		ResultSet rs = null;
+
 		try {
-		cn = Conexion.getInstancia().getConnection();
-		String query = "Select * from Categorias";
-		Statement st = cn.createStatement();
-		ResultSet rs = st.executeQuery(query);
-		
-		while (rs.next()) {
-			
-			Categoria categoria = new Categoria();
-			categoria.setIdCategoria(rs.getInt("IdCategoria"));
-			categoria.setNombre(rs.getString("Nombre"));
-			
-			lCategoria.add(categoria);
+			cn = Conexion.getInstancia().getConnection();
+			String query = "Select * from Categorias";
+			st = cn.createStatement();
+			rs = st.executeQuery(query);
+
+			while (rs.next()) {
+
+				Categoria categoria = new Categoria();
+				categoria.setIdCategoria(rs.getInt("IdCategoria"));
+				categoria.setNombre(rs.getString("Nombre"));
+
+				lCategoria.add(categoria);
 			}
 
-	
 		}
-		
-		catch (Exception e ) {
-			
+
+		catch (Exception e) {
+
 			e.printStackTrace();
+		} finally {
+			// agrego cierre de resulset y statement para que no queden conexiones abiertas
+			try {
+				if (rs != null)
+					rs.close();
+				if (st != null)
+					st.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
-		return lCategoria; 
+		return lCategoria;
 	}
 }
